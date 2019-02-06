0479d8e96ce41f3f9479ba7b9011e3de2da02cb0
KaustubhKekre <kkekre90@gmail.com>
2019-02-06 14:32:33 +0530

    added authentication

--- app/src/main/java/com/phantomorion/tsechacks/MainActivity.java
+++ app/src/main/java/com/phantomorion/tsechacks/MainActivity.java
@@ -1,13 +1,66 @@
 package com.phantomorion.tsechacks;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    public FirebaseAuth mAuth;
+    public FirebaseUser mUser;
+    public Uri uImg;
+    boolean connection;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        connection = haveNetworkConnection();
+        if (connection == false) {
+            Toast.makeText(MainActivity.this, "Please check your internet connection and try again", Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(MainActivity.this, NoInternet.class);
+            startActivity(intent);
+            finish();
+        } else {
+            setContentView(R.layout.activity_main);
+            mAuth = FirebaseAuth.getInstance();
+            mUser = mAuth.getCurrentUser();
+            if (mUser == null) {
+                startActivity(new Intent(MainActivity.this, signInOptions.class));
+                finish();
+            }
+            else
+            {
+
+            }
+        }
+        }
+
+
+
+    private boolean haveNetworkConnection() {
+        boolean haveConnectedWifi = false;
+        boolean haveConnectedMobile = false;
+
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo[] netInfo = cm.getAllNetworkInfo();
+        for (NetworkInfo ni : netInfo) {
+            if (ni.getTypeName().equalsIgnoreCase("WIFI"))
+                if (ni.isConnected())
+                    haveConnectedWifi = true;
+            if (ni.getTypeName().equalsIgnoreCase("MOBILE"))
+                if (ni.isConnected())
+                    haveConnectedMobile = true;
+        }
+        return haveConnectedWifi || haveConnectedMobile;
     }
-}
+    }
+
